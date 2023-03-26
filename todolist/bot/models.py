c2a107eb096c8fba350a758c03e3fa0535f575fd
mustafanysan <mustafa.nysan@mail.ru>
2023-03-26 20:36:15 +0600

    veiws

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -7,7 +7,8 @@ from todolist.core.models import User
 
 class TgUser(models.Model):
     """Класс модели для пользователя бота"""
-    chat_id = models.BigIntegerField(unique=True)
+    chat_id = models.BigIntegerField(verbose_name='Chat ID', unique=True)
+    username = models.CharField(verbose_name='Username', max_length=255, null=True, blank=True, default=None)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, default=None)
     verification_code = models.CharField(max_length=50, null=True, blank=True, default=None)
 
